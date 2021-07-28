76381c3d64e58229a513646a1f4b3dd0c558c4a3
Kevin Higgins <higgy714@gmail.com>
2021-07-28 12:15:40 -0400

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from "react-native";
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from "./LoadingComponent";
 
 const mapStateToProps = state => {
     return {
@@ -27,6 +28,31 @@ class About extends Component {
                 />
             );
         };
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+
         return (
             <ScrollView>
                 <Mission />
@@ -39,14 +65,14 @@ class About extends Component {
                     />
                 </Card>
             </ScrollView>
-        )
+        );
     }
 
 }
 
 const Mission = () => {
     return (
-        <Card>
+        <Card title="Our Mission">
             <Text style={{ margin: 10 }}>
                 We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
             </Text>
